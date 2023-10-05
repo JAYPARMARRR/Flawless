0f69221e5fb8956f491294ff5b29e9ac53941f04
Parmar Jay <jaykumarvinodbhi@gmail.com>
2023-10-05 16:13:42 +0530

    home Complit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,40 +1,44 @@
-// DEFAULT COLORS
-// rgb(251, 75, 122);
-// rgb(250, 45, 100);
-//  rgb(100, 100, 103);
-
 import "../Styles/Home.css";
 import { AiTwotoneShopping } from "react-icons/ai";
-import { FiLogIn } from 'react-icons/fi';
+// import { FiLogIn } from "react-icons/fi";
 
 const Home = () => {
   return (
     <>
-      <div className="Home_main-img h-[94vh]  ">
-
-        <nav className="flex justify-between items-center">
+      <div className="bg-cover bg-no-repeat bg-hero-lg h-[95vh]">
+        <nav className="flex justify-between items-center shadow-2xl ">
           <div>
-
-          <ul className=" flex text-white p-6 cursor-pointer">
-            <li className="block py-2 pl-3 pr-4  ">HOME</li>
-            <li className="block py-2 pl-3 pr-4">SHOP</li>
-            <li className="block py-2 pl-3 pr-4">ABOUT</li>
-            <li className="block py-2 pl-3 pr-4 ">CONTACT</li>
-          </ul>
+            <ul className=" flex text-white p-6 cursor-pointer">
+              <li className="block py-2 pl-3 pr-4  hover:text-darkPink ">HOME</li>
+              <li className="block py-2 pl-3 pr-4  hover:text-darkPink">SHOP</li>
+              <li className="block py-2 pl-3 pr-4  hover:text-darkPink">ABOUT</li>
+              <li className="block py-2 pl-3 pr-4  hover:text-darkPink">CONTACT</li>
+            </ul>
           </div>
-          <h1 className="text-white py-2 pl-3 pr-4 text-4xl">Flawless</h1>
+          <h1 className="text-white py-2 pl-3 pr-4 text-4xl cursor-pointer">
+            Flawless
+          </h1>
 
-
-          <FiLogIn className="text-yellow-400 "/>
-          <div className="flex text-white py-2 pl-3 pr-4">
-            <span >$44.98</span>
-            <AiTwotoneShopping />
+          <div className="flex text-white text-xl mx-32 gap-x-4 cursor-pointer ">
+            <span className="">$44.98</span>
+            <AiTwotoneShopping className="mt-[2px]" />
+            <span className="bg-white rounded-full h-4 w-4 text-xs my-[-1vh] mx-[-16px] justify-center items-center flex text-black hover:text-darkPink " >1</span>
           </div>
         </nav>
+
         <hr />
+        <div className="wrapper">
+
+        <p className="text-white mt-36 ml-16" >WELCOME TO FLAWLESS STORE</p>
+        <h1 className="text-white text-7xl mt-20 ml-14">The Best Skin</h1>
+        <h1 className="text-white text-7xl mt-4 ml-14"> Cleanser for You</h1>
+        <button className="border-white border px-4 py-3  text-white hover:bg-white hover:text-darkPink duration-500 ml-16 mt-11">SHOP NOW</button>
+        </div>
+
       </div>
     </>
   );
 };
 
 export default Home;
+ 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "hero-lg": "url('/src/assets/Home_main_Img.jpg')",
+      },
+      colors: {
+        midPink: "rgb(251, 75, 122)",
+        darkPink: "rgb(250, 45, 100)",
+        lightGray: "rgb(100, 100, 103)",
+      },
+      
+    },
   },
   plugins: [],
-}
-
+};

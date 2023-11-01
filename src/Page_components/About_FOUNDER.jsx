2dd62fe6348b8ebae3b60c5bea8d63c0ceedd16a
Parmar Jay <jaykumarvinodbhi@gmail.com>
2023-11-01 14:22:37 -0700

    about page

--- src/Page_components/About_FOUNDER.jsx
+++ src/Page_components/About_FOUNDER.jsx
@@ -1,33 +1,36 @@
-import About_FOUNDER_img from "../assets/about_immg.jpg"
-
-
+import About_FOUNDER_img from "../assets/about_immg.jpg";
 
 const About_FOUNDER = () => {
   return (
-    <div className="flex   h-[100vh]">
-      <div>
-        <img src={About_FOUNDER_img}  />
-      </div>
-
-      <div>
-
-        <div>
-
-        <p>FROM FOUNDER</p>
-        <p>Eget porttitor facilisi nunc placerat enim luctus arcu cursus habitant vulputate venenatis iaculis nunc et at viverra viverra sociis.</p>
-        <p>MILA CHRISTINE</p>
-        <p>Founder</p>
+    <>
+      <div className="flex justify-center items-cente  h-[100vh] w-full px-20">
+        <div className=" h-[35rem] w-[55rem]">
+          <img
+            src={About_FOUNDER_img}
+            className=" bg-cover bg-no-repeat  mt-24"
+          />
         </div>
 
-<div>
-<p>SOCIAL MEDIA</p>
-</div>
+        <div className="bg-slate-400 h-[35rem] w-[55rem] mt-20">
+          <div className=" flex justify-center items-center">
+            <p >FROM FOUNDER</p>
+            <p>
+              Eget porttitor facilisi nunc placerat enim luctus arcu cursus
+              habitant vulputate venenatis iaculis nunc et at viverra viverra 
+              sociis.
+            </p>
+            <p>MILA CHRISTINE</p>
+            <p>Founder</p>
+          </div>
 
+          <div>     
+            <p>SOCIAL MEDIA</p>
+          </div>
+          
+        </div>
       </div>
+    </>
+  );
+};
 
-
-    </div>
-  )
-}
-
-export default About_FOUNDER
+export default About_FOUNDER;
